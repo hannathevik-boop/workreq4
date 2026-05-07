27f6359809909b29ef3eca4132dd05c7effacb69
hannathevik-boop <hannathevik.com@gmail.com>
2026-05-07 20:06:33 +0200

    added meaningful queries titles for better readability and organization of the SQL file.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
+-- Query 1:
+-- List all books and the number of quotes for each
 SELECT
     Books.title,
     COUNT(Quotes.quote_id) AS number_of_quotes
@@ -5,6 +7,9 @@ FROM Books
 LEFT JOIN Quotes
 ON Books.book_id = Quotes.book_id
 GROUP BY Books.book_id;
+
+-- Query 2:
+-- List all authors and the number of quotes for each
 SELECT
     Authors.name,
     COUNT(Quotes.quote_id) AS number_of_quotes
@@ -12,6 +17,9 @@ FROM Authors
 LEFT JOIN Quotes
 ON Authors.author_id = Quotes.author_id
 GROUP BY Authors.author_id;
+
+-- Query 3:
+-- List all quotes for a specific book including quote text and author name
 SELECT
     Quotes.quote_text,
     Authors.name AS author_name
@@ -21,6 +29,9 @@ ON Quotes.book_id = Books.book_id
 JOIN Authors
 ON Quotes.author_id = Authors.author_id
 WHERE Books.title = 'Et dukkehjem';
+
+-- Query 4: 
+-- List all quotes for a specific author including quote text and book title
 SELECT
     Quotes.quote_text,
     Books.title AS book_title
